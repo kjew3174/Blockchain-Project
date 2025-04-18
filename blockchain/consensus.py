2610e97587d7191ebf7f087128fdcfe0be9d2bcc
박건률 <geonry1124@gmail.com>
2025-04-18 13:52:47 +0900

    앙기모띠

--- blockchain/consensus.py
+++ blockchain/consensus.py
@@ -4,3 +4,13 @@ def proof_of_work(block, difficulty):
             block.nonce += 1
             block.hash = block.calculate_hash()
         return block
+class Consensus:
+    def proof_of_stake(self, blockchain):
+          return True
+
+    def validate_proof(self, block: Block, previous_block: Block) -> bool:
+        if block.previous_hash != previous_block.hash:
+            return False
+        if block.hash != block.calculate_hash():
+            return False
+        return True

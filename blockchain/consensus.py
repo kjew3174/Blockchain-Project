be87bb70723389df5abba81416856cf956a0abb1
박건률 <geonry1124@gmail.com>
2025-04-18 13:59:13 +0900

    wlsdnEl

--- blockchain/consensus.py
+++ blockchain/consensus.py
@@ -1,12 +1,16 @@
-def proof_of_work(block, difficulty):
+from block import Block
+from typing import Any
+
+def proof_of_work(block: Block, difficulty):
         target = "0" * difficulty
         while block.hash[:difficulty] != target:
             block.nonce += 1
             block.hash = block.calculate_hash()
         return block
+
 class Consensus:
-    def proof_of_stake(self, blockchain):
-          return True
+    def proof_of_stake(self, blockchain: Any):
+        return True
 
     def validate_proof(self, block: Block, previous_block: Block) -> bool:
         if block.previous_hash != previous_block.hash:
